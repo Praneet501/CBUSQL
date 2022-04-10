cfd4d549048cd58f028ad1f728f0ce679d3ca19f
prabhjot325 <46024706+prabhjot325@users.noreply.github.com>
2022-04-10 18:52:40 -0300

    Update Search_Services.sql

--- Search_Services.sql
+++ Search_Services.sql
@@ -8,19 +8,21 @@ SELECT
 SS.service_name,
 SS.service_description, 
 FS.price,	
-FS.email, 
-FS.contact_number, 
-FS.is_avaiable, 
+FA.email, 
+FA.contact_number, 
+FS.is_available, 
 FS.service_location, 
 FS.rating
-
+  
 FROM freelance_services FS
+INNER JOIN freelance_account FA ON FA.freelance_id = FS.freelance_id
 INNER JOIN SERVICE SS ON SS.service_id = FS.service_id
 LEFT JOIN job_status JS ON JS.freelance_service_id = JS.freelance_service_id
+ 
   
-WHERE (SS.job_title like '%search_input%' 	
+WHERE (SS.service_name like '%search_input%' 	
 OR  SS.service_description = '%search_input%')
 AND FS.service_location = 'location_name'
 AND FS.is_available = 1
 AND FS.background_verified = 1
-AND JS.in_progress = FALSE  ;
+AND JS.in_progress = FALSE;
