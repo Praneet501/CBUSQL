522471cfcefd37f6ee23c1eecaf1b9702640d19c
prabhjot325 <46024706+prabhjot325@users.noreply.github.com>
2022-04-11 08:06:30 -0300

    Update Get_company_details_Freelancer.sql

--- Get_company_details_Freelancer.sql
+++ Get_company_details_Freelancer.sql
@@ -1,5 +1,5 @@
 /*
-Functionality: Get the list of company details those have more than one freelancer hired.
+Functionality: Get the list of company details those have zero freelancer hired.
 */
 
 SELECT Company_Name, COUNT(FA.freelance_id) as Total_Count, Sector_Detail, Company_Website
@@ -8,4 +8,4 @@ LEFT JOIN job_status JS ON CP.company_id = JS.company_id
 LEFT JOIN freelance_services FS ON FS.freelance_service_id = JS.freelance_service_id
 LEFT JOIN freelance_account FA ON FA.freelance_id = FS.freelance_id
 GROUP BY  company_name, Sector_detail, company_website
-HAVING COUNT(FA.freelance_id) > 1
+HAVING COUNT(FA.freelance_id) = 0
