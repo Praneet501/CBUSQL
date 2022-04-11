3b1f29fe4c205a84de01069f305dfe990616a6e1
prabhjot325 <46024706+prabhjot325@users.noreply.github.com>
2022-04-11 08:01:35 -0300

    Update and rename No_Experience_Education.sql to Experience_Education.sql

--- Experience_Education.sql
+++ Experience_Education.sql
@@ -1,12 +1,12 @@
 /*
 Name : Freelancer having no experience or no Education
-Functionality : Get list of Freelancer having no experience or Education for Background verification for Students
+Functionality : Get list of Freelancer having  experience or Education for Background verification for Students
 */
 	
-SELECT COUNT(EX.freelance_Id) as NO_Experience, COUNT(ED.freelance_Id) as NO_Education,  US.user_type FROM freelance_account FA
+SELECT COUNT(EX.freelance_Id) as Experienced, COUNT(ED.freelance_Id) as Education,  US.user_type FROM freelance_account FA
 	LEFT JOIN user_type US ON US.user_type_id = FA.user_type_id
 	LEFT JOIN education ED ON ED.freelance_Id = FA.freelance_Id
 	LEFT JOIN experience EX ON EX.freelance_Id = FA.freelance_Id
-		WHERE (Ed.education_id IS NULL OR EX.experience_id IS NULL)
+		WHERE (Ed.education_id IS NOT NULL OR EX.experience_id IS NOT NULL)
 		GROUP BY US.user_type												
         HAVING US.user_type = 'Student'
