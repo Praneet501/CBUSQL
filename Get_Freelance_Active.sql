8006cf3ac76e2aa89977b9285cdd5a4ec2024a67
prabhjot325 <46024706+prabhjot325@users.noreply.github.com>
2022-04-11 08:57:22 -0300

    Update Get_Freelance_Active.sql

--- Get_Freelance_Active.sql
+++ Get_Freelance_Active.sql
@@ -6,4 +6,5 @@ SELECT fc.freelance_id, fc.first_name, fc.last_name, fc.email, fs.job_title
 FROM freelance_account as fc
 INNER JOIN freelance_services as fs ON fc.freelance_id = fs.freelance_Id
 INNER JOIN service as se ON se.service_id = fs.service_id
-WHERE se.category = 'Electronic';
+WHERE se.category = 'Electronic'
+AND is_available = 1 AND background_verified = 1;
